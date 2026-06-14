c4bda470baf237e28f7812f4529d427f0f9e6ce7
Vadim Kalinnikov <moose@ylsoftware.com>
2026-06-14 16:47:24 +0300

    Added build_deb.sh

---
+++ build_deb.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+cd $(dirname $(readlink -f $0))
+
+PKG_VER=$(git describe --tags --always --dirty --match "v*")
+PKG_BUILD=1
+
+for distr in focal jammy noble resolute; do
+	echo "Building deb-package $PKG_VER-$PKG_BUILD for $distr"
+	echo "vadim-kalinnikov-wallpapers (${PKG_VER}-${PKG_BUILD}) ${distr}; urgency=medium
+
+  * New upstream release
+
+ -- Vadim Kalinnikov <moose@ylsoftware.com>  $(date --rfc-email)" > debian/changelog
+	dpkg-buildpackage -rfakeroot --build=source
+done
